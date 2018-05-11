395b2d1f225563fa3fc7765ece1caef8babc8529
Kiruba <Kiruba@email.com>
2018-05-11 12:08:02 +0530

    modified code.c

--- code/code.c
+++ code/code.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main()
 {
-char a[50];
-printf("Enter your name:");
-scanf("%s",a);
-printf("You Entered name is %s",a);
+	char a[50];
+int i;
+	printf("Enter your name:");
+	scanf("%s",a);
+	printf("You Entered name is %s\n",a);
+	for(i=0;a[i];i++)	
+	printf("%c\n",a[i]);
 }

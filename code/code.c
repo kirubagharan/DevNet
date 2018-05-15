f5e6fe4a2770019dc1b646b91dad232dd177a946
Kiruba <Kiruba@email.com>
2018-05-15 11:15:00 +0530

    code.c change in master

--- code/code.c
+++ code/code.c
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#include<stdio.h>
-int main()
-{
-	char a[50];
-int i;
-	printf("Enter your name:");
-	scanf("%s",a);
-	printf("You Entered name is %s\n",a);
-	for(i=0;a[i];i++)	
-	printf("%c\n",a[i]);
-}
-||||||| merged common ancestors
-=======
 #include<stdio.h>
 int main()
 {
@@ -29,4 +15,3 @@ int main()
 	}
 }
 //to check rebass comment
->>>>>>> master
